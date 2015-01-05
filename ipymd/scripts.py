9c25cb530fba33ba93bb95ef00609fb69f44c135
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-01-05 19:47:15 +0100

    WIP script.

--- ipymd/scripts.py
+++ ipymd/scripts.py
@@ -2,17 +2,26 @@ import argparse
 import os
 import os.path as op
 import glob
+import json
 from ipymd.converters import nb_to_markdown, markdown_to_nb
 
 def _flatten(l):
     return [item for sublist in l for item in sublist]
 
+def _to_skip(dirname):
+    return (op.basename(dirname).startswith('.') or
+            op.basename(dirname).startswith('_'))
+
 def _expand_dirs_to_files(files_or_dirs):
     files = []
     for file_or_dir in files_or_dirs:
         if op.isdir(file_or_dir):
+            # Skip dirnames starting with '.'
+            if _to_skip(file_or_dir):
+                continue
             # Recursively visit the directories and add the files.
-            files.extend(_expand_dirs_to_files(os.listdir(file_or_dir)))
+            files.extend(_expand_dirs_to_files([op.join(file_or_dir, file)
+                         for file in os.listdir(file_or_dir)]))
         elif '*' in file_or_dir:
             files.extend(glob.glob(file_or_dir))
         else:
@@ -26,6 +35,14 @@ def _file_has_extension(file, extensions):
 
 def _filter_files_by_extension(files, extensions):
     return [file for file in files if _file_has_extension(file, extensions)]
+
+def _read_md(file):
+    with open(file, 'r') as f:
+        return f.read()
+
+def _read_nb(file):
+    with open(file, 'r') as f:
+        return json.load(f)
 
 def main():
     parser = argparse.ArgumentParser(description=
@@ -45,16 +62,31 @@ def main():
                         help=('overwrite target file if it exists '
                               '(false by default)'))
 
+    # Parse the CLI arguments.
     args = parser.parse_args()
     files_or_dirs = args.files_or_dirs
     md_type = args.type or 'markdown'
     overwrite = args.overwrite
     convert_from = args.convert_from
 
+    # Find all files.
     files = _expand_dirs_to_files(files_or_dirs)
 
-    extensions = ['.ipymd' if convert_from == 'ipynb' else '.md']
-    files_to_convert = _filter_files_by_extension(files, extensions)
+    # Filter as a function of --from.
+    if convert_from == 'ipynb':
+        files = _filter_files_by_extension(files, '.ipynb')
+        conversion_function = nb_to_markdown
+        open_function = _read_nb
+    elif convert_from in ('md', 'markdown'):
+        files = _filter_files_by_extension(files, '.md')
+        conversion_function = markdown_to_nb
+        open_function = _read_md
+
+    for file in files:
+        print("Converting {0:s}...".format(file))
+
+        # contents = open_function(file)
+        # conversion_function(contents)
 
 if __name__ == '__main__':
     main()
