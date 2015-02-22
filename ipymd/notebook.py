f7c65ac09f4a99e4a22f52fe171ecab6de506365
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-02-22 16:54:54 +0100

    WIP: notebook reader.

--- ipymd/notebook.py
+++ ipymd/notebook.py
@@ -8,12 +8,46 @@
 
 
 #------------------------------------------------------------------------------
+# Utility functions
+#------------------------------------------------------------------------------
+
+def _ensure_string(source):
+    """Ensure a source is a string."""
+    if isinstance(source, list):
+        input = '\n'.join([line.rstrip() for line in source])
+    else:
+        input = source
+    return input.rstrip()
+
+
+def _cell_output(cell):
+    """Return the output of an ipynb cell."""
+    outputs = cell.get('outputs', [])
+    # Add stdout.
+    output = ('\n'.join(_ensure_string(output.get('text', ''))
+                        for output in outputs)).rstrip()
+    # Add text output.
+    output += ('\n'.join(_ensure_string(output.get('data', {}).
+                                        get('text/plain', []))
+                         for output in outputs)).rstrip()
+    return output
+
+
+#------------------------------------------------------------------------------
 # Notebook reader
 #------------------------------------------------------------------------------
 
 class NotebookReader(object):
+    """Reader for notebook cells.
+
+    nbformat v4 only."""
     def read(self, ipynb_cells):
-        pass
+        for ipynb_cell in ipynb_cells:
+            # TODO: cell type?
+            cell = {'cell_type': None}
+            output = _cell_output(ipynb_cell)
+            assert output
+            yield cell
 
 
 #------------------------------------------------------------------------------
