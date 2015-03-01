47c886c0352c3288ea35ff253186b092d9fe8355
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-03-01 18:31:48 +0100

    Fixing setup.py.

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ from setuptools import setup
 setup(
     name='ipymd',
     version='0.1.0-dev',
-    packages=['ipymd'],
+    packages=['ipymd',
+              'ipymd.tests',
+              'ipymd.formats',
+              'ipymd.formats.tests',
+              ],
     entry_points={
         'console_scripts': [
             'ipymd=ipymd.scripts:main',
